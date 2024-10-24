bebd760dc9d702abd01ff9ed69254761442afbfb
Nabi Tohidi <nabi@gmail.com>
2024-10-24 13:46:47 +0000

    script3 changed by Nabi

--- script3.sh
+++ script3.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 if [ $# -eq 3 ]; then
-	echo "All Arguments are $@"
 	echo "Number of Arguments are $#"
 	echo "Script PID is $$"
 else
 	echo "Insufficient variable numbers..."
-	echo "Child PID is $!"
-	exit 1;
 fi
