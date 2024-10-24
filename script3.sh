06bc1ae31d335d092e691f1856389ff0ae89907e
Arash Foroughi <arashforoughi@outlook.com>
2024-10-24 17:15:20 +0330

    script3 edited

--- script3.sh
+++ script3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 3 ]; then
-	echo "All Arguments are $@"
+#	echo "All Arguments are $@"
 	echo "Number of Arguments are $#"
-	echo "Script PID is $$"
+#	echo "Script PID is $$"
 else
-	echo "Insufficient variable numbers..."
+#	echo "Insufficient variable numbers..."
 	echo "Child PID is $!"
 	exit 1;
 fi
